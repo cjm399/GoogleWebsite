c0cddcef519c5c51e53d8c9bb1f2b55a80408bca
Chris McCole <chrismccole1@gmail.com>
2020-12-17 17:38:31 -0500

    new build

--- buildScript.sh
+++ buildScript.sh
@@ -1,1 +1,1 @@
-find . -type f -name \*.html -o -name \*.js -o -name \*.css -exec sed -i s/PLACEHOLDER/${VARIABLE}/g {} \;
+find . -type f -name \*.html -o -name \*.js -o -name \*.css -exec sed -i s/GOOGLE_API_KEY/${GOOGLE_API_KEY}/g {} \;
